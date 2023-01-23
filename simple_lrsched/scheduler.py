c1e61ebf8c6199db36716bedd948523d25870abd
Karl Semich <0xloem@gmail.com>
2023-01-23 14:17:00 -0500

    filled part of an outline point

--- simple_lrsched/scheduler.py
+++ simple_lrsched/scheduler.py
@@ -3,13 +3,13 @@ import torch, torch.optim
 from .loss import Loss
 
 class SimpleLR(torch.optim._LRScheduler):
-    def __init__(self, optimizer, use_diff=True, history_size=1024, degree=3, last_epoch=-1, verbose=False):
+    def __init__(self, optimizer, use_diff=True, past_size=1024, degree=3, last_epoch=-1, verbose=False):
         super(SimpleLR, self).__init__(optimizer, last_epoch, verbose)
         self.loss = Loss(self.optimizer)
         self.degree = degree
         self.use_diff = use_diff
-        self._N_VARS = 1 # up this
-        assert history_size >= self._N_VARS * degree
+        self._N_VARS = 3
+        assert past_size >= self._N_VARS * degree
         # this creates a tensor containing the variable exponent combinations in a polynomial.
         # like: [
         #  [0, 0, 0] x^0y^0z^0 = 1
@@ -20,16 +20,17 @@ class SimpleLR(torch.optim._LRScheduler):
         #  [2, 2, 2] = (x^2)(y^2)(z^2)
         # ]
         exp_idcs = torch.arange(degree ** self._N_VARS)
-        self.var_exps = torch.stack(
+        self._var_exps = torch.stack(
                 [
                     exp_idcs.div(degree**var_idx, rounding_mode='floor') % degree
                     for var_idx in range(self._N_VARS)
                 ]
                 dim=-1,
             )
-        self.history = torch.empty((history_size, self._N_VARS))
-        self.history_idx = 0
-        if use_diff:
+        self._past = torch.empty((past_size, self._N_VARS))
+        self._past_idx = len(self._past)
+        self._past_needed = self._past_idx - len(self._var_exps)
+        if self.use_diff:
             self.last_loss = [None] * len(self.optimizer.param_groups)
             self.last_test = [None] * len(self.optimizer.param_groups)
     def __del__(self):
@@ -43,6 +44,7 @@ class SimpleLR(torch.optim._LRScheduler):
         for idx in range(len(self.optimizer.param_groups)):
             loss_value = None
             test_value = 0
+            assert self.last_loss[idx] != loss[idx] # if this doesn't pass due to logic error we could just reuse last lr. if it doesn't pass due to coincidence and quantization then the assert would be removed. it might make sense to add some subrandom value for quantization issues.
             if use_diff:
                 if self.last_loss is not None:
                     loss_value = loss[idx] - self.last_loss[idx]
@@ -58,11 +60,23 @@ class SimpleLR(torch.optim._LRScheduler):
             if test_value is not None:
                 loss_value += test_value * test_value
 
-            loss_value is now the value to minimize by setting the learning rate.
-            1 - store history based on the last learning rate and loss_value
-            2 - if there is enough history, do a least squares fit of a polynomial to it
+            #loss_value is now the value to minimize by setting the learning rate.
+            #1 - store past based on the last learning rate and loss_value
+            if self._past_idx > 0:
+                self._past_idx -= 1
+            else:
+                self._past[1:] = self._past[:-1]
+            new_past_row = self._past[self._past_idx]
+            new_past_row[0] = loss_value
+            new_past_row[1] = # get last lr
+            new_past_row[2] = # get step
+
+            if self._past_idx <= self._past_needed:
+                2 - if there is enough past, do a least squares fit of a polynomial to it
+
                 3 - then minimize the polynomial given the current loss or step number to calc the lr
-            2b - otherwise, try a random and small lr to gather data
+            else:
+                2b - otherwise, try a random and small lr to gather data
 
             lrs.append(lr)
 
